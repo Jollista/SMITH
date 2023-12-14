eb42b300533c52bc2e332eb5065b74f45e488e84
Sulfurous-Impersonation <june@watson-net.com>
2023-12-14 10:21:02 -0800

    Porting over datapool commands

--- src/commands.js
+++ src/commands.js
@@ -3,12 +3,84 @@
  * and registration.
  */
 
-export const AWW_COMMAND = {
-  name: 'awwww',
-  description: 'Drop some cuteness on this channel.',
+import * as fs from 'fs';
+
+function getCategoryChoices(directory) 
+{
+  var choices = [];
+  var files = fs.readdirSync(directory)
+  
+  //for each file in directory
+  for (let file of files) {
+    console.log("file name : " + file.split(".")[0]);
+
+    choices.push({
+      name: capitalize(file.split(".")[0]),
+      value: file,
+    });
+  }
+  return choices;
+}
+
+//dice rolling command
+export const ROLL_COMMAND = {
+  name: 'roll',
+  description: 'Roll a d10',
+  options: [
+    {
+      type: 3,
+      name: 'type',
+      description: 'Type of dice to roll',
+      required: true,
+      choices: [{name:"d10", value:"d10"}, {name:"d6", value:"d6"}]
+    },
+    {
+      type: 4,
+      name: 'modifier',
+      description: 'Modifier to a d10 roll, or the number of d6s to roll',
+      required: false,
+    },
+    {
+      type: 3,
+      name: 'label',
+      description: 'Label of the roll',
+      required: false,
+    },
+  ],
+  type: 1,
 };
 
-export const INVITE_COMMAND = {
-  name: 'invite',
-  description: 'Get an invite link to add the bot to your server',
+export const RULE_COMMAND = {
+  name: 'rule',
+  description: 'Look up a rule',
+  options: [
+    {
+      type: 3,
+      name: 'name',
+      description: 'The category of rule you want to look up',
+      required: true,
+    },
+  ],
+  type: 1,
+};
+
+export const ITEM_COMMAND = {
+  name: 'item',
+  description: 'Look up an item',
+  options: [
+    {
+      type: 3,
+      name: 'category',
+      description: 'The type of item you want to look up',
+      required: true,
+      choices: getCategoryChoices('./src/datapool/items'),
+    },
+    {
+      type: 3,
+      name: 'name',
+      description: 'The name of the item',
+      required: true,
+    },
+  ],
+  type: 1,
 };
