749ff8f166054a6bdf5947b9e650da00a276267a
Sulfurous-Impersonation <june@watson-net.com>
2023-12-13 16:42:40 -0800

    Struggling to add lookup slash commands

--- commands.js
+++ commands.js
@@ -1,42 +1,25 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
 import { capitalize, InstallGlobalCommands } from './utils.js';
+import * as fs from 'fs';
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
+function getCategoryChoices(directory) 
+{
+  var choices = [];
+  var files = fs.readdirSync(directory)
+  for (let file of files) {
+    console.log("file name : " + file.split(".")[0]);
+    choices.push({
+      name: capitalize(file.split(".")[0]),
+      value: file.split(".")[0].toLowerCase(),
     });
   }
-
-  return commandChoices;
+  return choices;
 }
 
 // Simple test command
 const TEST_COMMAND = {
   name: 'test',
   description: 'Basic command',
-  type: 1,
-};
-
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
   type: 1,
 };
 
@@ -54,6 +37,36 @@ const ROLL_COMMAND = {
   type: 1,
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, ROLL_COMMAND];
+const RULE_COMMAND = {
+  name: 'lookup rule',
+  description: 'Look up a rule',
+  options: [
+    {
+      type: 3,
+      name: 'category',
+      description: 'The category of rule you want to look up',
+      required: true,
+      choices: getCategoryChoices('./datapool/rules/'),
+    }
+  ],
+  type: 1,
+};
+
+const ITEM_COMMAND = {
+  name: 'lookup item',
+  description: 'Look up an item',
+  options: [
+    {
+      type: 3,
+      name: 'type',
+      description: 'The type of item you want to look up',
+      required: true,
+      choices: getCategoryChoices('./datapool/items/'),
+    }
+  ],
+  type: 1,
+};
+
+const ALL_COMMANDS = [TEST_COMMAND, ROLL_COMMAND, RULE_COMMAND, ITEM_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
