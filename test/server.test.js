8f331ded2fda3b3d36ed503d29f1157fbdbef05d
Sulfurous-Impersonation <june@watson-net.com>
2023-12-18 14:54:57 -0800

    Slightly better test file

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,20 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
+import {
+  InteractionResponseType,
+  InteractionType,
+  InteractionResponseFlags,
+} from 'discord-interactions';
+import sinon from 'sinon';
 import server from '../src/server.js';
+import * as dotenv from 'dotenv';
+import { ROLL_COMMAND } from '../src/commands.js';
+
+dotenv.config({path: './.dev.vars'});
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const env = {SUPABASE_URL, SUPABASE_KEY};
+console.log(process.env.SUPABASE_URL);
 
 describe('Server', () => {
   describe('GET /', () => {
@@ -15,6 +29,40 @@ describe('Server', () => {
       const body = await response.text();
 
       expect(body).to.equal('👋 123456789');
+    });
+  });
+
+  describe('POST /', () => {
+    let verifyDiscordRequestStub;
+
+    beforeEach(() => {
+      verifyDiscordRequestStub = sinon.stub(server, 'verifyDiscordRequest');
+    });
+
+    afterEach(() => {
+      verifyDiscordRequestStub.restore();
+    });
+
+    it('should handle a PING interaction', async () => {
+      const interaction = {
+        type: InteractionType.PING,
+      };
+
+      const request = {
+        method: 'POST',
+        url: new URL('/', 'http://discordo.example'),
+      };
+
+      //const env = {SUPABASE_URL, SUPABASE_KEY};
+
+      verifyDiscordRequestStub.resolves({
+        isValid: true,
+        interaction: interaction,
+      });
+
+      const response = await server.fetch(request, env);
+      const body = await response.json();
+      expect(body.type).to.equal(InteractionResponseType.PONG);
     });
   });
 });
