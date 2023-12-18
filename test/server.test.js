ad554bf5f33c2acfa5a4c2185c92d40a892796a0
Sulfurous-Impersonation <june@watson-net.com>
2023-12-18 15:13:41 -0800

    Lint appeasement

--- test/server.test.js
+++ test/server.test.js
@@ -1,20 +1,6 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import {
-  InteractionResponseType,
-  InteractionType,
-  InteractionResponseFlags,
-} from 'discord-interactions';
-import sinon from 'sinon';
 import server from '../src/server.js';
-import * as dotenv from 'dotenv';
-import { ROLL_COMMAND } from '../src/commands.js';
-
-dotenv.config({path: './.dev.vars'});
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
-const env = {SUPABASE_URL, SUPABASE_KEY};
-console.log(process.env.SUPABASE_URL);
 
 describe('Server', () => {
   describe('GET /', () => {
